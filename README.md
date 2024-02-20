a80b35784ffdc97ab9f7c6c984bd3d5a43049fcb
Mat Mice <MatMice@mail.com>
2024-02-19 18:36:20 -0600

    working on more html/css support

--- README.md
+++ README.md
@@ -5,5 +5,13 @@
 - Secure storage of HTML,CSS,JS (No injection attacks please!)
 - Save/Load from local storage
 - Kid friendly tools for developing the page(s)
-- Secret key registration
+- Secret key registration (Must have secret key to register accounts/pages and key changes daily for newly created users)
 - Database for perm storage of page(s)
+- Max file size per page ( 2MB to start? )
+- Javascript support (This will be hard, implemented after full HTML5/CSS3 support is working and secure)
+- UI tools for drag and drop development
+- AI tools for image generations and code snippets
+- Auto Ad detection and removal in linting proccess (And in the case that kids need/want to host ads we perhaps setup a non targeted ad campaign that shows safe advertisments to users accepting viewing IE its off by default and only used as opt-in)
+- Video and Image linting for NSFW
+- Detect NSFW text/content on page before upload
+- Smart contract to host the entire service with direct donation tool to fund the project
--- index.js
+++ index.js
@@ -1,126 +1,130 @@
-// Made with the help of ChatGPT 3.5
+	// Made with the help of ChatGPT 3.5
+	
+	const express = require('express');
+	const bodyParser = require('body-parser');
+	const createDOMPurify = require('dompurify');
+	const validator = require('validator');
+	const helmet = require('helmet');
+	const { JSDOM } = require('jsdom');
+	
+	
+	const app = express();
+	const port = 3000;
+	
+	const window = new JSDOM('').window;
+	const DOMPurify = createDOMPurify(window);
+	
+	app.use(helmet());
+	app.use(bodyParser.urlencoded({ extended: true })); 
+	app.use(express.static('public'));
+	app.use((err, req, res, next) => {
+    	console.error(err.stack);
+    	res.status(500).send('Internal Server Error');
+	});
+	
+	const storedSnippets = {};
+	
+	app.get('/:username', (req, res) => {
+    	const username = req.params.username;
+    	const snippet = storedSnippets[username];
+    	console.log(snippet)
+    	if (snippet) {
+        	res.send(`
+            	<!DOCTYPE html>
+            	<html lang="en">
+            	<head>
+                	<meta charset="UTF-8">
+                	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+                	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+                	<title>${username}</title>
+                	<style>
+                		${snippet.css}
+                	</style>
+            	</head>
+            	<body>
+                	<div id="${username}">
+                    	${snippet.html}
+                	</div>
+                	<style>
+                		${snippet.css}
+                	</style>
+                	<script>
+                		${snippet.js}
+                	</script>
+            	</body>
+            	</html>
+        	`);
+    	} else {
+        	res.status(404).send('User not found');
+    	}
+	});
+	
+	app.get('/', (req, res) => {
+    	res.sendFile(__dirname + '/public/index.html');
+	});
+	
+	app.post('/register', (req, res) => {
+    const username = req.body.username;
+    let htmlSnippet = req.body.htmlSnippet;
+    let cssSnippet = req.body.cssSnippet;
+    let jsSnippet = req.body.jsSnippet;
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const createDOMPurify = require('dompurify');
-const validator = require('validator');
-const helmet = require('helmet');
-const { JSDOM } = require('jsdom');
+    // Sanitize the snippets using DOMPurify if necessary
 
+    // Your existing validation and storage logic here
 
-const app = express();
-const port = 3000;
-
-const window = new JSDOM('').window;
-const DOMPurify = createDOMPurify(window);
-
-app.use(helmet());
-app.use(bodyParser.urlencoded({ extended: true })); 
-app.use(express.static('public'));
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Internal Server Error');
-});
-
-const storedSnippets = {};
-
-app.get('/:username', (req, res) => {
-    const username = req.params.username;
-    const snippet = storedSnippets[username];
-    console.log(snippet)
-    if (snippet) {
-        res.send(`
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>MatMice-like Web Maker</title>
-                <style>
-                    /* Your styles here */
-                </style>
-            </head>
-            <body>
-                <div id="${username}">
-                    ${snippet}
-                </div>
-                <button onclick="goToRandomPage()">Go to random page</button>
-                <script>
-                    // Function to navigate to a random page
-                     function goToRandomPage() {
-        fetch('/random/page')
-            .then(response => {
-            	console.log(response);
-                if (response.ok) {
-               		return response.url;
-                    //return response.text();
-                } else {
-                    throw new Error('Failed to fetch random page');
-                }
-            })
-            .then(url => {
-            	//console.log(username);
-                if (url !== '') {
-                    // Redirect to the randomly selected page
-                    window.location.href = url;
-                } else {
-                    alert('No pages available');
-                }
-            })
-            .catch(error => {
-                console.error(error);
-                alert('Failed to fetch random page');
-            });
-    }
-                </script>
-            </body>
-            </html>
-        `);
-    } else {
-        res.status(404).send('User not found');
-    }
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
-
-app.post('/register', (req, res) => {
-console.log(req)
-      const username = req.body.username;
-    let snippet = req.body.snippet;
-    // Sanitize the snippet using DOMPurify
-    snippet = DOMPurify.sanitize(snippet);
-
-console.log("text")
-console.log(username)
-console.log(snippet)
     if (storedSnippets[username]) {
         res.status(409).send('Username already taken');
     } else {
-        storedSnippets[username] = snippet;
+        storedSnippets[username] = {
+            html: htmlSnippet,
+            css: cssSnippet,
+            js: jsSnippet
+        };
         res.redirect(`/${username}`);
     }
-});
-// New endpoint to handle random page request
-app.get('/random/page', (req, res) => {
-    const usernames = Object.keys(storedSnippets);
-    if (usernames.length > 0) {
-        const randomUsername = usernames[Math.floor(Math.random() * usernames.length)];
-
-        // Validate the randomUsername before redirection
-        if (validator.isAlphanumeric(randomUsername)) {
-            res.redirect(`/${randomUsername}`);
-        } else {
-            res.status(500).send('Invalid username for redirection');
-        }
-    } else {
-        res.status(404).send('No pages available');
-    }
-});
-
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+});/*
+	app.post('/register', (req, res) => {
+	console.log(req)
+      	const username = req.body.username;
+    	let snippet = req.body.snippet;
+    	console.log("HERE")
+    	console.log(snippet);
+    	// Sanitize the snippet using DOMPurify
+    	snippet = DOMPurify.sanitize(snippet, {
+    		ALLOWED_TAGS: ['b', 'i', 'em', 'strong', 'a','style'],
+    		ALLOWED_ATTR: ['href', 'style'], // Allow the 'style' attribute for CSS
+    		FORBID_ATTR: ['onerror', 'onload'], // Forbid specific attributes
+		});
+	console.log("text")
+	console.log(username)
+	console.log(snippet)
+    	if (storedSnippets[username]) {
+        	res.status(409).send('Username already taken');
+    	} else {
+        	storedSnippets[username] = snippet;
+        	res.redirect(`/${username}`);
+    	}
+	});*/
+	// New endpoint to handle random page request
+	app.get('/random/page', (req, res) => {
+    	const usernames = Object.keys(storedSnippets);
+    	if (usernames.length > 0) {
+        	const randomUsername = usernames[Math.floor(Math.random() * usernames.length)];
+	
+        	// Validate the randomUsername before redirection
+        	if (validator.isAlphanumeric(randomUsername)) {
+            	res.redirect(`/${randomUsername}`);
+        	} else {
+            	res.status(500).send('Invalid username for redirection');
+        	}
+    	} else {
+        	res.status(404).send('No pages available');
+    	}
+	});
+	
+	
+	app.listen(port, () => {
+    	console.log(`Server running at http://localhost:${port}`);
+	});
+	
