b9e43be9734063e202ca8ca94b066307bc61937d
Mat Mice <MatMice@mail.com>
2024-02-26 02:22:59 -0600

    saving and some organizing on sites page

---
+++ public/javascript/home.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('.save-btn').forEach(button => {
+    button.addEventListener('click', function() {
+        const url = this.getAttribute('data-url');
+        const filename = this.getAttribute('data-filename');
+        saveAsFile(url, filename);
+    });
+});
+
+function saveAsFile(url, filename) {
+    fetch(url)
+        .then(response => response.text())
+        .then(html => {
+            const element = document.createElement('a');
+            element.setAttribute('href', 'data:text/html;charset=utf-8,' + encodeURIComponent(html));
+            element.setAttribute('download', filename);
+
+            element.style.display = 'none';
+            document.body.appendChild(element);
+
+            element.click();
+
+            document.body.removeChild(element);
+        })
+        .catch(console.error);
+}
